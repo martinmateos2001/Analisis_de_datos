a2f9ec5886609abc7baee6492f70e20dfb7aa12f
Martin Mateos <martinmateos_2001@hotmail.com>
2025-11-02 19:06:30 -0300

    Pude mostrar el primer caracter : )

--- TP2/TP-02-ComillasDobles.py
+++ TP2/TP-02-ComillasDobles.py
@@ -1,2 +1,20 @@
 # -*- coding: utf-8 -*-
 
+#%% importamos las librerias
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#%% PRUEBA - Intento ver el primer caracter.
+
+df_caracteres = pd.read_csv('kuzushiji_full.csv')
+
+primero = df_caracteres.iloc[0, [i for i in range(784)]] 
+#selecciono todas las columnas menos label y selecciono la fila cero
+
+primero = np.array(primero).reshape((28, 28))
+#Transformo a matriz
+
+plt.imshow(primero, cmap='gray')
+plt.show
